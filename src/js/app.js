30ed796bbe448e6abf86e2c5ab22545b018cc84c
fahim <therunninghorse00@gmail.com>
2023-11-12 03:59:42 +0600

    word pages are added with data fetching.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
+const API_URL = "http://127.0.0.1:8000/api";
 
+async function allWords() {
+  try {
+    const res = await axios.get(`${API_URL}/words/`);
+    return res.data.reverse();
+  } catch (error) {
+    return [];
+  }
+}
