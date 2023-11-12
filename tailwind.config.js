9b50d3284ccbd9bcdbdac33c9ac3b2d062e1b7ad
fahim <therunninghorse00@gmail.com>
2023-11-12 17:47:10 +0600

    small fix in the tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/pages/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./src/pages/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {},
   },
